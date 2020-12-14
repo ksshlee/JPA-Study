74d816b8aec7f4f3a5b8cec33744f5a3a2b5a20c
ksshlee <ksshlee@naver.com>
2020-12-14 23:40:17 +0900

    method 로 query 만들기 실습

--- src/main/java/com/study/jpa/entity/Post.java
+++ src/main/java/com/study/jpa/entity/Post.java
@@ -1,7 +1,6 @@
 package com.study.jpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -9,13 +8,25 @@ import java.util.Set;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id @GeneratedValue
     private Long id;
 
-    @Setter
+    private String title;
+
+    @Lob
     private String content;
+
+    private Long likeCounts;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<Comment> comments;
+
+    @Builder
+    public Post(String title, String content, Long likeCounts){
+        this.title = title;
+        this.content = content;
+        this.likeCounts = likeCounts;
+    }
 }
--- src/main/java/com/study/jpa/repository/PostRepository.java
+++ src/main/java/com/study/jpa/repository/PostRepository.java
@@ -4,6 +4,14 @@ package com.study.jpa.repository;
 import com.study.jpa.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+import java.util.List;
 
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByContentContainsIgnoreCase(String keyword);
+
+    Long countByLikeCountsIsGreaterThanEqual(Long count);
+
+    List<Post> findByContentContainsIgnoreCaseAndLikeCountsGreaterThanEqual(String keyword,Long likeCount);
+
+    List<Post> findByContentContainsIgnoreCaseAndLikeCountsGreaterThanEqualOrderByLikeCountsDesc(String keyword,Long likeCount);
 }
